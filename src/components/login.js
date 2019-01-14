6879551f68f8c3b40e8794bc883befd143a974e3
nethermore11 <nethermore11@gmail.com>
2019-01-14 16:27:52 +0700

    store added

---
+++ src/components/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import pic from '../logo.svg'
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <img src={pic} alt={'aaaa'}/>
+            </div>
+        )
+    }
+}
+
+export default Login;
