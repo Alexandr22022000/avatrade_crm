b474c70435d8f9570feb07bcf5177a4eaeea67e8
Alex <zol22222@mail.ru>
2019-01-31 00:50:28 +0700

    Add ueers filter

--- src/personal/actions/types.js
+++ src/personal/actions/types.js
@@ -4,5 +4,6 @@ export const SET_RANKS  = 'SET_RANKS';
 export const UPDATE_USER_DATA = 'UPDATE_USER_DATA';
 export const SET_EMPTY_USER = 'SET_EMPTY_USER';
 export const DEL_CURRENT_USER = 'DEL_CURRENT_USER';
+export const SET_USERS_FILTER = 'SET_USERS_FILTER';
 
 export const CHANGE_BLOCK_STATUS = 'CHANGE_BLOCK_STATUS';
--- src/personal/containers/Stuff.js
+++ src/personal/containers/Stuff.js
@@ -2,25 +2,20 @@ import {connect} from "react-redux";
 import Stuff from "../components/Stuff";
 import {getStuff} from "../async-actions/getStuff";
 import {getPerson} from "../async-actions/getPerson";
-import updateUserData from "../async-actions/updateUserData";
-import {addNewUser} from "../async-actions/addNewUser";
 import {setEmptyCurrentUser} from "../actions/setEmptyCurrentUser";
+import setUsersFilter from "../actions/setUsersFilter";
 
 
 export default connect(
     state => ({
         users: state.stuff.users,
         reqStatus: state.status.requestSuccessful,
+        showAll: state.status.showAll,
     }),
     dispatch => ({
-        onGetUsers: (is_all = false) => dispatch(getStuff(is_all)),
+        onGetUsers: () => dispatch(getStuff()),
         onGetCurrentUser: (id) => dispatch(getPerson(id)),
-        onUpdateUserData: (data) => {
-            dispatch(updateUserData(data))
-        },
-        onAddNewUser: (userData) => {
-            dispatch(addNewUser(userData));
-        },
         onSetEmptyUser: () => dispatch(setEmptyCurrentUser()),
+        setUsersFilter: (showAll) => dispatch(setUsersFilter(showAll)),
     })
 )(Stuff);
