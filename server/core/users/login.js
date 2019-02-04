c363426a5829cb3f8b0b6e217977c091d00f2b89
Alex <zol22222@mail.ru>
2019-02-04 13:54:49 +0700

    Check prmissions count in login

--- server/core/users/login.js
+++ server/core/users/login.js
@@ -5,7 +5,7 @@ module.exports = (login, password) => {
     return new Promise((resolve, reject) => {
         query(QUERY.LOGIN, [login, password])
             .then(({rows}) => {
-                if (rows.length === 0) return resolve({isUser: false});
+                if (rows.length === 0 || rows[0].permissions.length === 0) return resolve({isUser: false});
 
                 resolve({isUser: true, data: {permissions: rows[0].permissions, id: rows[0].id}});
             });
