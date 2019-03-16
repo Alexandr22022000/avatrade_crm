46214d1738a293d83c21dc0efa7bfe27a07802f8
Alex <zol22222@mail.ru>
2019-03-16 14:08:57 +0700

    Add new get stocks api

---
+++ server/cargos/api/v0.0/get_stocks_new.js
@@ -0,0 +1,55 @@
+const {checkUser} = require('neuronex-login-backend'),
+    {query} = require('neuronex-pg'),
+    QUERY = require('../../pSQL/stocks');
+
+module.exports = (app) => {
+    app.get('/api/v0.1/stocks', (req, res) => {
+        const user = checkUser(req.query.token);
+
+        if (!user) return res.status(401).end();
+
+        let search = req.query.search;
+        if (!search || search.trim() === '' || search === 'null')
+            search = null;
+        else
+            search = "%" + search.trim() + "%";
+
+        let store_id = !req.query.store || req.query.store === 'null' ? null : req.query.store,
+            sows_all = req.query.is_all || true;
+
+        query(QUERY.GET_STOCKS, [search, store_id, sows_all])
+            .then(({rows}) => {
+                const cargos = [];
+
+                rows.forEach(stock => {
+                    let isNew = true;
+                    cargos.forEach(cargo => {
+                        if (cargo.id === stock.cargo_id) {
+                            isNew = false;
+
+                            cargo.stocks.push({
+                                count: stock.count,
+                                store: stock.store,
+                                store_id: stock.store_id,
+                            });
+                        }
+                    });
+
+                    if (isNew) {
+                        cargos.push({
+                            id: stock.cargo_id,
+                            name: stock.name,
+                            article: stock.article,
+                            stocks: [{
+                                count: stock.count,
+                                store_id: stock.store_id,
+                                store: stock.store,
+                            }],
+                        });
+                    }
+                });
+
+                res.status(200).json({stocks: cargos});
+            });
+    });
+};
