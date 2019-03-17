5c9f908c96279c27eb35de1ed2fe41ef12fc6472
nethe <nethermore11@gmail.com>
2019-03-17 11:21:57 +0700

    services fastServices editor added

--- src/cashbox/async-actions/getFastServices.js
+++ src/cashbox/async-actions/getFastServices.js
@@ -3,7 +3,7 @@ import setFastServices from "../actions/setFastServices";
 
 const getFastServices = () => (dispatch, getState) => {
     HTTPS.get('/api/v0.0/fast_services',{},dispatch,getState)
-        .then(response => {console.log(response);dispatch(setFastServices(response.fast_services))});
+        .then(response => {dispatch(setFastServices(response.fast_services))});
 };
 
 export default getFastServices;
