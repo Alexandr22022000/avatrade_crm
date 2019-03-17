35129a2007ab7aa324ab69a4d53d4aad88147ae0
Alex <zol22222@mail.ru>
2019-03-17 16:26:02 +0700

    Add services server validation

--- server/services/api/v0.0/add_service.js
+++ server/services/api/v0.0/add_service.js
@@ -2,7 +2,8 @@ const {checkUser} = require('neuronex-login-backend'),
     {query} = require('neuronex-pg'),
     checkPermissions = require('../../../core/users/checkPermissions'),
     PERMISSIONS = require('../../../core/constants/permissions'),
-   QUERY = require('../../pSQL/services');
+    QUERY = require('../../pSQL/services'),
+    QUERY_CARGOS = require('../../pSQL/cargos');
 
 
 
@@ -11,14 +12,27 @@ module.exports = (app) => {
         let name = req.body.name,
             price = req.body.price,
             is_product = req.body.is_product,
-            consumables = {consumables: req.body.consumables};
+            consumables = req.body.consumables;
 
         const user = checkUser(req.body.token);
 
         if (!user) return res.status(401).end();
         if (!checkPermissions(user, [PERMISSIONS.TOP_MANAGER, PERMISSIONS.OWNER])) return res.status(403).end();
 
-        query(QUERY.ADD_SERVICES, [Date.now(), name, is_product, price, consumables])
-            .then(() => res.status(200).end());
+        query(QUERY_CARGOS.GET)
+            .then(({rows}) => {
+                consumables = consumables.filter(consumable => {
+                    for (let key in rows) {
+                        if (+rows[key].id === +consumable.id && consumable.count > 0) return true;
+                    }
+
+                    return false;
+                });
+
+                consumables = {consumables: consumables};
+
+                query(QUERY.ADD_SERVICES, [Date.now(), name, is_product, price, consumables])
+                    .then(() => res.status(200).end());
+            });
     });
 };
