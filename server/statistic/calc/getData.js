a20309b83697938cd1bdca671eefc4a1f86ae9b4
Alex <zol22222@mail.ru>
2019-04-06 11:46:21 +0700

    Test timezone

--- server/statistic/calc/getData.js
+++ server/statistic/calc/getData.js
@@ -35,7 +35,7 @@ module.exports = (start, end) => {
         let i = 1;
         while (endLoop.getTime() <= end.getTime()) {
 
-            query('SELECT SUM(sales.price) AS price, SUM(sales.price_resell) AS price_resell, sales.store_id, sales.is_card, stores.name FROM sales JOIN stores ON sales.store_id = stores.id WHERE sales.id >= $1 AND sales.id < $2 GROUP BY sales.store_id, sales.is_card, stores.name', [changeTimezone(startLoop), changeTimezone(endLoop)])
+            query('SELECT SUM(sales.price) AS price, SUM(sales.price_resell) AS price_resell, sales.store_id, sales.is_card, stores.name FROM sales JOIN stores ON sales.store_id = stores.id WHERE sales.id >= $1 AND sales.id < $2 GROUP BY sales.store_id, sales.is_card, stores.name', [startLoop, endLoop])
                 .then(callback(i, ({rows}, err, i) => {
                     data.sells.push({
                         day: i,
@@ -43,7 +43,7 @@ module.exports = (start, end) => {
                     });
                 }));
 
-            query('SELECT SUM(cashbox_collection.value) AS value, cashbox_collection.store_id, stores.name FROM cashbox_collection JOIN stores ON cashbox_collection.store_id = stores.id WHERE cashbox_collection.id >= $1 AND cashbox_collection.id < $2 GROUP BY cashbox_collection.store_id, stores.name', [changeTimezone(startLoop), changeTimezone(endLoop)])
+            query('SELECT SUM(cashbox_collection.value) AS value, cashbox_collection.store_id, stores.name FROM cashbox_collection JOIN stores ON cashbox_collection.store_id = stores.id WHERE cashbox_collection.id >= $1 AND cashbox_collection.id < $2 GROUP BY cashbox_collection.store_id, stores.name', [startLoop, endLoop])
                 .then(callback(i, ({rows}, err, i) => {
                     data.collections.push({
                         day: i,
@@ -51,7 +51,7 @@ module.exports = (start, end) => {
                     });
                 }));
 
-            query('SELECT SUM(sales.price) AS price, SUM(sales.price_resell) AS price_resell, sales.user_id, users.name FROM sales JOIN users ON sales.user_id = users.id WHERE sales.id >= $1 AND sales.id < $2 GROUP BY sales.user_id, users.name', [changeTimezone(startLoop), changeTimezone(endLoop)])
+            query('SELECT SUM(sales.price) AS price, SUM(sales.price_resell) AS price_resell, sales.user_id, users.name FROM sales JOIN users ON sales.user_id = users.id WHERE sales.id >= $1 AND sales.id < $2 GROUP BY sales.user_id, users.name', [startLoop, endLoop])
                 .then(callback(i, ({rows}, err, i) => {
                     data.user_sells.push({
                         day: i,
@@ -64,35 +64,35 @@ module.exports = (start, end) => {
             i++;
         }
 
-        query('SELECT SUM(price) AS price, SUM(price_resell) AS price_resell, store_id FROM sales WHERE id < $1 AND is_card = false GROUP BY store_id', [changeTimezone(start)])
+        query('SELECT SUM(price) AS price, SUM(price_resell) AS price_resell, store_id FROM sales WHERE id < $1 AND is_card = false GROUP BY store_id', [start])
             .then(callback(null, ({rows}) => {
                 data.oldSells = rows;
             }));
 
-        query('SELECT SUM(value) AS value, store_id FROM cashbox_collection WHERE id < $1 GROUP BY store_id', [changeTimezone(start)])
+        query('SELECT SUM(value) AS value, store_id FROM cashbox_collection WHERE id < $1 GROUP BY store_id', [start])
             .then(callback(null, ({rows}) => {
                 data.oldCollections = rows;
             }));
 
-        query('SELECT users.id, users.name, users.status, ranks.payment FROM users JOIN ranks ON users.rank = ranks.id WHERE users.id < $1 ', [changeTimezone(end)])
+        query('SELECT users.id, users.name, users.status, ranks.payment FROM users JOIN ranks ON users.rank = ranks.id WHERE users.id < $1 ', [end])
             .then(callback(null, ({rows}) => {
                 data.users = rows.filter(item => item.status === 0);
                 data.inactiveUsers = rows.filter(item => item.status === 1);
             }));
 
-        query('SELECT id, name FROM stores WHERE status = 0 AND id < $1', [changeTimezone(end)])
+        query('SELECT id, name FROM stores WHERE status = 0 AND id < $1', [end])
             .then(callback(null, ({rows}) => {
                 data.stores = rows;
             }));
 
-        query('SELECT workdays.id, workdays.user_id, workdays.calendar, workdays.values, workdays.descriptions, users.name FROM workdays JOIN users ON workdays.user_id = users.id WHERE workdays.date >= $1 AND workdays.date < $2', [changeTimezone(start), changeTimezone(end)])
+        query('SELECT workdays.id, workdays.user_id, workdays.calendar, workdays.values, workdays.descriptions, users.name FROM workdays JOIN users ON workdays.user_id = users.id WHERE workdays.date >= $1 AND workdays.date < $2', [start, end])
             .then(callback(null, ({rows}) => {
                 data.workdays = rows.filter((item) => item.calendar === 0);
                 data.increase_kpi = rows.filter((item) => item.calendar === 3);
                 data.decrease_kpi = rows.filter((item) => item.calendar === 4);
             }));
 
-        query('SELECT payments.id, payments.user_id, payments.value, users.name FROM payments JOIN users ON payments.user_id = users.id WHERE payments.date >= $1 AND payments.date < $2', [changeTimezone(start), changeTimezone(end)])
+        query('SELECT payments.id, payments.user_id, payments.value, users.name FROM payments JOIN users ON payments.user_id = users.id WHERE payments.date >= $1 AND payments.date < $2', [start, end])
             .then(callback(null, ({rows}) => {
                 data.payments = rows;
             }));
