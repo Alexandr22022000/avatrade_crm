f1c8b476a892cd8254570d8ef62b001b3976fcc6
Alex <zol22222@mail.ru>
2019-04-06 12:06:47 +0700

    Test timezone

--- server/statistic/calc/getMonth.js
+++ server/statistic/calc/getMonth.js
@@ -3,14 +3,14 @@ module.exports = (data) => {
         end = new Date(data),
         days = new Date(data);
 
-    start.setDate(2);
+    start.setDate(1);
     start.setHours(0);
     start.setMinutes(0);
     start.setSeconds(0);
     start.setMilliseconds(0);
 
     end.setMonth(end.getMonth() + 1);
-    end.setDate(2);
+    end.setDate(1);
     end.setHours(0);
     end.setMinutes(0);
     end.setSeconds(0);
